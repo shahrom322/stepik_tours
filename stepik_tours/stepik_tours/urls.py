400fceed8824ccdee4755dc7702e43006153ee92
shahrom322 <ololoev05@gmail.com>
2021-11-15 16:54:47 +0300

    Created mock-file with context data. Created main_view, departure_view, tour_view.Removed duplicate template parts in base.html

--- stepik_tours/stepik_tours/urls.py
+++ stepik_tours/stepik_tours/urls.py
@@ -8,8 +8,8 @@ handler500 = custom_handler500
 
 
 urlpatterns = [
+    path('departure/<str:departure>', departure_view, name='departure'),
+    path('tour/<int:id>', tour_view, name='tour'),
     path('admin/', admin.site.urls),
-    path('', main_view),
-    path('/departure/<str:departure>', departure_view),
-    path('/tour/<int:id>', tour_view),
+    path('', main_view, name='home'),
 ]
